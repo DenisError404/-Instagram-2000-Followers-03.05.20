be719d2bab11c0eab25f0c924277ad8a06ab45b1
Denis Rafi <62852838+DenisError404@users.noreply.github.com>
2020-05-03 03:01:01 +0100

    Add files via upload

---
+++ -Instagram-2000-Followers-03.05.20.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _2000_Followers
+{
+    class Program
+    {
+        static void Main()
+        {
+            DateTime aDate = DateTime.Now;
+
+            Console.WriteLine("By Denis Rafi");
+            Console.WriteLine("--------------");
+
+            Console.WriteLine("2000 Followers Thank You");
+            Console.WriteLine("---------------------------");
+
+            Console.WriteLine(aDate.ToString("dddd, dd MMMM yyyy HH:mm:ss"));
+            Console.WriteLine("-----------------------------");
+
+            Console.ReadKey();
+        }       
+    }
+}
